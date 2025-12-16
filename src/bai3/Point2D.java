f34c5d7f381bd65b1338a31971f8199549164f01
Phan Minh Thi <thi@corsivalab.space>
2025-12-16 21:29:06 +0700

    pointer 2d pointer 3d

---
+++ src/bai3/Point2D.java
@@ -0,0 +1,42 @@
+package bai3;
+
+public class Point2D {
+    private float x, y = 0f;
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D() {}
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public float[] getXY(){
+        return new float[]{x, y};
+    }
+
+    public String toString(){
+        return "Point2D{" + "x=" + x + ", y=" + y + '}';
+    }
+}
---
+++ src/bai3/Point3D.java
@@ -0,0 +1,37 @@
+package bai3;
+
+public class Point3D extends Point2D{
+    private float z = 0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {}
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z){
+        super.setXY(x, y);
+        this.z = z;
+    }
+
+    public float[] getXYZ(){
+        float[] arr = new float[3];
+        arr = super.getXY();
+        arr[2] = z;
+        return arr;
+    }
+
+    @Override
+    public String toString(){
+        return "Point3D{" + super.toString() + ", z=" + z + '}';
+    }
+}
